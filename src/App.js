e9d5c8d92eb6560b8f3f5c44d476087fcef9d767
Adhikarla Venkata Sekhar <126749603+SekharAdhikarla@users.noreply.github.com>
2024-03-22 08:08:50 +0530

    Accordian Firest

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React from "react";
+import Accordion from "./componentes/Accordion";
+import "./componentes/Accordion.css";
 
-import React from 'react'
-import Accordian from './componentes/Accordian'
-const App = () => {
+export default function App() {
   return (
-    <div>
-   <Accordian/>
-      
+    <div className="App">
+      <h1>Hello sekharx</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <Accordion title="First Accordion" content="First Accordion content" />
+        <Accordion
+          title="Second Accordion"
+          content="Second Accordion content"
+        />
+        <Accordion
+          title="Third Accordion"
+          content=" <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>"
+        />
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
